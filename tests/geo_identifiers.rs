05a3bf23e9708df966a9db1543d3462c07581000
Zac <zac@cmdrvl.com>
2026-09-02 15:53:36 -0400

    bd-67wx: normalize identifier test formatting

--- tests/geo_identifiers.rs
+++ tests/geo_identifiers.rs
@@ -1,12 +1,12 @@
 #![forbid(unsafe_code)]
 
-use canon::geo::{compare_property_sets, GeoEntityLevel};
 use canon::geo::{
-    diff_tile_identifier_vintages, normalize_nyc_bbl, registry_entries_for_clusters,
-    registry_proposal_from_ledger_json, GeoIdentifierCluster, GeoIdentifierErrorCode,
-    GeoIdentifierTombstone, GeoPropertyDocumentAssertion, GeoPropertySetRelation,
-    GeoTileIdentifierVintage, CANON_GEO_REGISTRY_PROPOSAL_VERSION, GEO_BBL_NORMALIZATION_RULE_ID,
+    CANON_GEO_REGISTRY_PROPOSAL_VERSION, GEO_BBL_NORMALIZATION_RULE_ID, GeoIdentifierCluster,
+    GeoIdentifierErrorCode, GeoIdentifierTombstone, GeoPropertyDocumentAssertion,
+    GeoPropertySetRelation, GeoTileIdentifierVintage, diff_tile_identifier_vintages,
+    normalize_nyc_bbl, registry_entries_for_clusters, registry_proposal_from_ledger_json,
 };
+use canon::geo::{GeoEntityLevel, compare_property_sets};
 use serde_json::Value;
 use std::collections::{BTreeMap, BTreeSet};
 
@@ -447,14 +447,18 @@ fn t58_registry_proposal_from_ledger_rows_preserves_denominator_and_skips_reach_
     assert_eq!(assertion.loan_id, "loan-a");
     assert_eq!(assertion.parcel_ids.len(), 2);
     assert_eq!(assertion.building_ids.len(), 1);
-    assert!(assertion
-        .parcel_ids
-        .iter()
-        .all(|id| id.starts_with("cmdrvl:parcel:")));
-    assert!(assertion
-        .building_ids
-        .iter()
-        .all(|id| id.starts_with("cmdrvl:building:")));
+    assert!(
+        assertion
+            .parcel_ids
+            .iter()
+            .all(|id| id.starts_with("cmdrvl:parcel:"))
+    );
+    assert!(
+        assertion
+            .building_ids
+            .iter()
+            .all(|id| id.starts_with("cmdrvl:building:"))
+    );
 
     let serialized = serde_json::to_value(&proposal).expect("proposal serializes");
     assert_no_probabilistic_fields(&serialized);
